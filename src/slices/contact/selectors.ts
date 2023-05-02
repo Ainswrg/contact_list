7d506c328620596e2829759e74c57c003ff8806b
JaslanD <jaslan.dlnv@gmail.com>
2023-05-02 17:26:50 +0300

    feat: add selectContatsStatus

--- src/slices/contact/selectors.ts
+++ src/slices/contact/selectors.ts
@@ -1,4 +1,5 @@
-import { type ContactItem } from 'shared'
+import { type TypeStatus, type ContactItem } from 'shared'
 import type { RootState } from '../../app/store'
 
 export const selectContacts = (state: RootState): ContactItem[] => state.contact.list
+export const selectContactsStatus = (state: RootState): TypeStatus => state.contact.status
